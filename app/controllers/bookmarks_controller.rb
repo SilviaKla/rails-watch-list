3054012e8b282a111cf325d0987fdba3efd52d68
silvia pasini <silvia.pasini03@gmail.com>
2022-08-19 18:15:33 +0200

    apply

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,34 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  before_action :set_list, only: %i[new create]
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :list_id, :movie_id)
+  end
+end
